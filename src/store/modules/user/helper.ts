aaa5dc093a01d5fa757019b4061a244ed4b6ea04
iwooji <107770766@qq.com>
2023-06-13 18:32:57 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://prompt.iwooji.com:22321/avata1.png',
+      name: 'iwooji',
+      description: 'Star on <a href="https://chat.iwooji.com:11977" class="text-blue-500" target="_blank" >iChatGPT</a>',
     },
   }
 }
